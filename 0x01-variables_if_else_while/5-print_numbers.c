f3f2905398f42bc39e79a8c4e7ab8f216ba185d9
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-09 16:15:06 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints all single numbers of base ten starting from zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
+
