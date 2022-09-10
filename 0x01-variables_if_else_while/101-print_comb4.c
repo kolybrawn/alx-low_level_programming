57b344226e0e3b6dbf63ede080f2b1a396676186
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-09 18:00:08 -0700

    leaves

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,2 +1,43 @@
 #include <stdio.h>
-`
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print numbers from zero to ninety nine
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+
+	while (i <= '7');
+	{
+		while (j <= '8');
+		{
+			while (k <= '9');
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+				k++;
+			}
+			k = '0';
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
