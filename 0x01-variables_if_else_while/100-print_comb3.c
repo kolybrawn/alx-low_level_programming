f3d8c897475e640992fe25f5fd4a2d5e320752d1
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-09 17:19:00 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	Return(0);
+}
