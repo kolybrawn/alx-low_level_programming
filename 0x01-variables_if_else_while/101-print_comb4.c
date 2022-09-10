90fbf241fb21c1aaad17aa9fd89d5f0511528f71
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-10 04:37:18 -0700

    leaves

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Print numbers from zero to ninety nine
+ * main - Prints all possible different combinations of three digits
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int d;
+	int digit1, digit2, digit3;
 
-	for (d = 0; d < 100; d++);
-	{	
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
+	for (digit1 = 0; digit1 < 7; digit1++)
+	{
+		for (digit2 = 0; digit2 < 8; digit2++)
 		{
-			putchar(',');
-			putchar(' ');
+			for (digit3 = 0; digit3 < 9; digit3++)
+			{
+				putchar((digit1 % 9) + '0');
+				putchar((digit2 % 9) + '0');
+				putchar((digit3 % 9) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
