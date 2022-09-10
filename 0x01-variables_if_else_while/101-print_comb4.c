46dbfc971a062ff2a3502905e4832dc9d5841e39
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-10 04:41:46 -0700

    leaves

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ int main(void)
 		{
 			for (digit3 = 0; digit3 < 10; digit3++)
 			{
-				putchar((digit1 % 9) + '0');
-				putchar((digit2 % 9) + '0');
-				putchar((digit3 % 9) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
