1bb36fe8b6cabd6b8a2f0134cf10cf0f47f6f365
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-13 21:01:40 -0700

    nested

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 if character is lowercase, 0 of otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * header - file containing all functions used in this current directory
+ * header file containing all functions used in this current directory
  * file: main.h
  */
 
 int _putchar(char c);
-void print_aplhabet(void);
-void print_alphabe_x10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
