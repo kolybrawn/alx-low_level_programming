7259ac998cb758e7aa90e60e93074753391bab6c
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-13 19:52:37 -0700

    nested

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * header - file containing all functions used in this current directory
+ * file: main.h
+ */
+
+int _putchar (char c);
+void print_aplhabet(void);
+void print_alphabe_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
