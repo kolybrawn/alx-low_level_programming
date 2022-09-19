8a22972323873c891ecf3dbfaa4ff7f64ded42a5
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-19 12:48:09 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
+ * main - generates random passwords for 101-crackme
  * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
+	int sum;
+	char c;
 
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 	return (0);
-
 }
