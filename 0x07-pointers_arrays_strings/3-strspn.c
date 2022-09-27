680c827f2221aa397f40facfb5588d34e4dcbc96
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-26 20:04:06 -0700

    strings at work

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 
 {
 	unsigned int i, j;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -18,7 +19,6 @@ unsigned int _strspn(char *s, char *accept)
 		j = 0;
 		while (accept[j] != '\0' && s[i] != accept[j])
 		j++;
-
 		if (accept[j] == '\0')
 		return (i);
 		i++;
