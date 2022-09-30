916cd8c6ab7283dd98a086eeadddd8ea79b15549
kolawole ibrahim <kolybrawn@gmail.com>
2022-09-30 16:57:38 -0700

    arg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,24 +3,6 @@
 #include <stdlib.h>
 
 /**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-	for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-	if (isdigit(argv[i][j]) == 0)
-	return (1);
-	return (0);
-}
-
-/**
  * main - Prints the minimum number of coins
  * to make change for an amount of cents.
  * @argc: argument count
