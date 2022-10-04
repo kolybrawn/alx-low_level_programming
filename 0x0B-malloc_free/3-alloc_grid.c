adc79b71f5168b2cdd7258ed953edcf54af53ab5
kolawole ibrahim <kolybrawn@gmail.com>
2022-10-04 15:48:28 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,32 +9,37 @@
  * Return: pointer to 2-D integer grid
  */
 
-int **alloc_grid(int width, int height)
+int **alloc_grid(int width, int height}
 {
-	int i, j;
-	int **p;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	i = j = 0;
-	if (height < 1)
+	if (width <= 0 || height <= 0)
 	return (NULL);
-	p = (int **)malloc(height * sizeof(p));
-	if (p == NULL)
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+	return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+	
+		if (twoD[hgt_index] == NULL)
 		{
-			free(p);
+			for (; hgt_index >= 0; hgt_index--)
+			free(twoD[hgt_index]);
+
+			free(twoD);
 			return (NULL);
 		}
-	for (i = 0; i < height; i++)
-		{
-		p[i] = malloc(width * sizeof(int));
-		if (p[i] == NULL)
-			{
-				for (j = 0; j < i; j++)
-				free(p[j]);
-				free(p);
-				return (NULL);
-			}
-	for (j = 0; j < width; j++)
-	p[i][j] = 0;
-		}
-	return (p);
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+		twoD[hgt_index][wid_index] = 0;
+	}
+	return (twoD);
 }
